84c70391868e7ac2eb3278ad9d3e7cd76df340f7
jjain31 <jjain312002@gmail.com>
2024-11-11 23:16:16 +0530

    complete files

--- DevTinder Backend/src/config/database.js
+++ DevTinder Backend/src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose= require("mongoose");
 const connectDb=async()=>{
-          await mongoose.connect("mongodb+srv://jainjainam3102:TxEqqSwAmWwKG7SI@namasteworld.stxtv.mongodb.net/DevTinder")
+          await mongoose.connect("mongodb+srv://{database_id}:{password}@namasteworld.stxtv.mongodb.net/DevTinder")
 }
 module.exports=connectDb;
